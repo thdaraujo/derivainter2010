44a91fe25e449e1d83641d6c53a69f0a64b69264
worf.mogh@gmail.com <worf.mogh@gmail.com@67eb9d07-7ef5-34f6-de95-7a3d825eb28a>
2010-11-12 01:54:20 +0000

    .live não funcionou, nem .bind. Achei que o jspf dava um append da mensagem de erro mas, pelo visto, faz outra coisa. Deve fazer uma macumba. Preciso de um handler de javascript que lide com despachos!

--- trunk/Deriva/Deriva/web/scripts/script.js
+++ trunk/Deriva/Deriva/web/scripts/script.js
@@ -1,27 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function(){     
 
 	/* Converting the #box div into a bounceBox: */
 	$('#box').bounceBox();
 
-	/* Listening for the click event and toggling the box: */
+        	/* Listening for the click event and toggling the box: */
 	$('a.button').click(function(e){
-
 		$('#box').bounceBoxToggle();
 		e.preventDefault();
 	});
+
+        $('a.button').live('teste', function(e){
+		$('#box').bounceBoxToggle();
+		e.preventDefault();
+	});        
         
 	/* When the box is clicked, hide it: */
 	$('#botao').click(function(){
 		$('#box').bounceBoxHide();
             });
-
-        $('body').bind('teste', function(){
-            alert('fuck')
-        });
-       
-
-//        $(window).unload(function(){
-//            $('#box').bounceBoxHide();
-//	});
-        
 });
+
+
+
